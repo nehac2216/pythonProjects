6b50b14dc15dff8acfea5c4237a1c87e9170a27e
nehac2216 <84366621+nehac2216@users.noreply.github.com>
2021-06-06 23:07:00 +0530

    Add files via upload

---
+++ str_format.py
@@ -0,0 +1,20 @@
+# The Format Method Substitution
+# Type1-
+name= 'neha'
+age= 23
+print('{0} is {1} years old'.format(name,age))
+
+# Type2-
+name= 'titu'
+age= 19
+print('{name} is {age} years old'.format(name=name,age=age))
+
+# Type3-
+name= 'shinuu'
+age= 22
+print('{} is {} years old'.format(name,age))
+
+# Type4-
+name= 'asif'
+age= 25
+print(f'{name} is {age} years old')
